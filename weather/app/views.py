0fcc1b2d0eb837ee3bdb911277e3e61345f295c1
Dan <dan.streif@gmail.com>
2023-11-17 16:08:14 +0100

    Add weather information

--- weather/app/views.py
+++ weather/app/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from dotenv import load_dotenv
+import os
+import requests
 
 def index(request):
-    return render(request, 'app/index.html')
+    load_dotenv()
+    
+    API_KEY = os.getenv('OPEN_WEATHER_API')
 
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=' + API_KEY
+
+    city = 'Berlin'
+    
+    city_weather = requests.get(url.format(city)).json()
+    
+    print(city_weather)
+
+    weather = {
+        'city' : city,
+        'temperature' : city_weather['main']['temp'],
+        'description' : city_weather['weather'][0]['description'],
+        'icon' : city_weather['weather'][0]['icon']
+    }
+
+    context = {'weather' : weather}
+
+    return render(request, 'app/index.html', context)
